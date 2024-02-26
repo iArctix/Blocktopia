89daac82917b716c48f386981fa1e9a733a84fdf
iArctix <connortodd.ct@gmail.com>
2024-02-26 02:24:04 +0000

    Revert "Revert "Movement Working Finally""
    This reverts commit d42dcd9f8170c50c057ac546a1ba5b9e98236448.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Check if player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f, groundLayer);
+
+        // Player movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
