411267a7caaf13ab6cd20fb579133fecb9e8288c
iArctix <connortodd.ct@gmail.com>
2024-03-04 16:10:54 +0000

    Upgrade System Working
    Added all the upgrades and Ui functionality behind them

--- Assets/Scripts/BlacksmithMenu.cs
+++ Assets/Scripts/BlacksmithMenu.cs
@@ -19,7 +19,13 @@ public class BlacksmithMenu : MonoBehaviour
     public GameObject pickaxearrow;
     public GameObject axearrow;
 
+
     public TextMeshProUGUI Toolinfo;
+    public TextMeshProUGUI Oreneeded;
+    public TextMeshProUGUI Woodneeded;
+    public TextMeshProUGUI coinsneeded;
+
+    
 
     public 
     
@@ -85,6 +91,31 @@ public class BlacksmithMenu : MonoBehaviour
         axearrow.SetActive(false);
         //Info
         Toolinfo.text = " The bow should be used for ranged combat. Its Current Level is  " + stats.bowlevel;
+
+        if(stats.bowlevel == 1)
+        {
+            Oreneeded.text = "Copper Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Oak Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.bowlevel == 2)
+        {
+            Oreneeded.text = "Gold Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Birch Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if(stats.bowlevel == 3)
+        {
+            Oreneeded.text = "Titanium Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Ash Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if(stats.bowlevel == 4)
+        {
+            Oreneeded.text = "Max Level";
+            Woodneeded.text = "Max Level";
+            coinsneeded.text = "Max Level";
+        }
     }
     void sword()
     {
@@ -96,6 +127,32 @@ public class BlacksmithMenu : MonoBehaviour
         axearrow.SetActive(false);
         //Info
         Toolinfo.text = " The sword should be used for melee combat its current level is" + stats.swordlevel;
+
+        if (stats.swordlevel == 1)
+        {
+            Oreneeded.text = "Copper Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Oak Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.swordlevel == 2)
+        {
+            Oreneeded.text = "Gold Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Birch Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.swordlevel == 3)
+        {
+            Oreneeded.text = "Titanium Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Ash Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.swordlevel == 4)
+        {
+            Oreneeded.text = "Max Level";
+            Woodneeded.text = "Max Level";
+            coinsneeded.text = "Max Level";
+        }
+
     }
     void axe()
     {
@@ -107,6 +164,31 @@ public class BlacksmithMenu : MonoBehaviour
         axearrow.SetActive(true);
         //Info
         Toolinfo.text = " The axe can chop down certain trees for resources the current tier is  " + stats.axelevel;
+
+        if (stats.axelevel == 1)
+        {
+            Oreneeded.text = "Copper Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Oak Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.axelevel == 2)
+        {
+            Oreneeded.text = "Gold Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Birch Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.axelevel == 3)
+        {
+            Oreneeded.text = "Titanium Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Ash Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.axelevel == 4)
+        {
+            Oreneeded.text = "Max Level";
+            Woodneeded.text = "Max Level";
+            coinsneeded.text = "Max Level";
+        }
     }
     void pickaxe()
     {
@@ -118,8 +200,185 @@ public class BlacksmithMenu : MonoBehaviour
         axearrow.SetActive(false);
         //Info
         Toolinfo.text = " The pickaxe can mine certain ores for resources the current tier is  " + stats.pickaxelevel;
+
+        if (stats.pickaxelevel == 1)
+        {
+            Oreneeded.text = "Copper Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Oak Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.pickaxelevel == 2)
+        {
+            Oreneeded.text = "Gold Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Birch Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.pickaxelevel == 3)
+        {
+            Oreneeded.text = "Titanium Required " + inventory.Copper + " / " + "20";
+            Woodneeded.text = " Ash Required " + inventory.Oak + " / " + "20";
+            coinsneeded.text = " Coins Required " + inventory.coins + " / " + "200";
+        }
+        else if (stats.pickaxelevel == 4)
+        {
+            Oreneeded.text = "Max Level";
+            Woodneeded.text = "Max Level";
+            coinsneeded.text = "Max Level";
+        }
     }
 
+    public void upgradebutton()
+    {
+        if (currenttool == 1)
+        {
+            if (stats.bowlevel == 1)
+            {
+                if(inventory.Copper >= 20 && inventory.Oak >= 20 && inventory.coins >= 200)
+                {
+                    stats.bowlevel++;
+                    inventory.Copper -= 20;
+                    inventory.Oak -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.bowlevel == 2)
+            {
+                if (inventory.gold >= 20 && inventory.Birch >= 20 && inventory.coins >= 200)
+                {
+                    stats.bowlevel++;
+                    inventory.gold -= 20;
+                    inventory.Birch -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.bowlevel == 3)
+            {
+                if (inventory.titanium >= 20 && inventory.Ash >= 20 && inventory.coins >= 200)
+                {
+                    stats.bowlevel++;
+                    inventory.titanium -= 20;
+                    inventory.Ash -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.bowlevel == 4)
+            {
+                Debug.Log("Max Level");
+            }
+        }
+        else if (currenttool == 2)
+        {
+            if (stats.swordlevel == 1)
+            {
+                if (inventory.Copper >= 20 && inventory.Oak >= 20 && inventory.coins >= 200)
+                {
+                    stats.swordlevel++;
+                    inventory.Copper -= 20;
+                    inventory.Oak -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.swordlevel == 2)
+            {
+                if (inventory.gold >= 20 && inventory.Birch >= 20 && inventory.coins >= 200)
+                {
+                    stats.swordlevel++;
+                    inventory.gold -= 20;
+                    inventory.Birch -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.swordlevel == 3)
+            {
+                if (inventory.titanium >= 20 && inventory.Ash >= 20 && inventory.coins >= 200)
+                {
+                    stats.swordlevel++;
+                    inventory.titanium -= 20;
+                    inventory.Ash -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.swordlevel == 4)
+            {
+                Debug.Log("Max Level");
+            }
+        }
+        else if (currenttool == 3)
+        {
+            if (stats.pickaxelevel == 1)
+            {
+                if (inventory.Copper >= 20 && inventory.Oak >= 20 && inventory.coins >= 200)
+                {
+                    stats.pickaxelevel++;
+                    inventory.Copper -= 20;
+                    inventory.Oak -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.pickaxelevel == 2)
+            {
+                if (inventory.gold >= 20 && inventory.Birch >= 20 && inventory.coins >= 200)
+                {
+                    stats.pickaxelevel++;
+                    inventory.gold -= 20;
+                    inventory.Birch -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.pickaxelevel == 3)
+            {
+                if (inventory.titanium >= 20 && inventory.Ash >= 20 && inventory.coins >= 200)
+                {
+                    stats.pickaxelevel++;
+                    inventory.titanium -= 20;
+                    inventory.Ash -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.pickaxelevel == 4)
+            {
+                Debug.Log("Max Level");
+            }
+        }
+        else if (currenttool == 4)
+        {
+            if (stats.axelevel == 1)
+            {
+                if (inventory.Copper >= 20 && inventory.Oak >= 20 && inventory.coins >= 200)
+                {
+                    stats.axelevel++;
+                    inventory.Copper -= 20;
+                    inventory.Oak -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.axelevel == 2)
+            {
+                if (inventory.gold >= 20 && inventory.Birch >= 20 && inventory.coins >= 200)
+                {
+                    stats.axelevel++;
+                    inventory.gold -= 20;
+                    inventory.Birch -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.axelevel == 3)
+            {
+                if (inventory.titanium >= 20 && inventory.Ash >= 20 && inventory.coins >= 200)
+                {
+                    stats.axelevel++;
+                    inventory.titanium -= 20;
+                    inventory.Ash -= 20;
+                    inventory.coins -= 200;
+                }
+            }
+            else if (stats.axelevel == 4)
+            {
+                Debug.Log("Max Level");
+            }
+        }
+    }
+    
 
 
 }
