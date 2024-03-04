f61d3c499e9d26ddcdb78a24d46802f76727964f
iArctix <connortodd.ct@gmail.com>
2024-03-04 17:28:26 +0000

    Inventory fully working
    Added script implementation and such now works as intended

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject inventoryui;
+    public GameObject gameplayui;
 
-    // Update is called once per frame
+    public TextMeshProUGUI Bow;
+    public TextMeshProUGUI Sword;
+    public TextMeshProUGUI Pick;
+    public TextMeshProUGUI Axe;
+
+    public TextMeshProUGUI Coins;
+
+    public TextMeshProUGUI Copper;
+    public TextMeshProUGUI Gold;
+    public TextMeshProUGUI Titanium;
+
+    public TextMeshProUGUI Oak;
+    public TextMeshProUGUI Birch;
+    public TextMeshProUGUI Ash;
+
+    public InventoryData inv;
+    public Playerstats stats;
+    
+        
     void Update()
     {
-        
+        Bow.text = "Lev " + stats.bowlevel;
+        Sword.text ="Lev " + stats.swordlevel;
+        Pick.text = "Lev " + stats.pickaxelevel;
+        Axe.text ="Lev " + stats.axelevel;
+
+        Coins.text = inv.coins.ToString();
+
+        Copper.text = inv.Copper.ToString();
+        Gold.text = inv.gold.ToString();
+        Titanium.text = inv.titanium.ToString();
+
+        Oak.text = inv.Oak.ToString();
+        Birch.text = inv.Birch.ToString();
+        Ash.text = inv.Ash.ToString();
+
+        if (Input.GetKeyDown(KeyCode.I) && !inventoryui.activeSelf)
+        {
+            inventoryui.SetActive(true);
+            gameplayui.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.I) && inventoryui.activeSelf)
+        {
+            inventoryui.SetActive(false);
+            gameplayui.SetActive(true);
+        }
+
+
     }
 }
