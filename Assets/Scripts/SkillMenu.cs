8d037fad83218300b7043e0f4b5fe90377bb7aca
iArctix <connortodd.ct@gmail.com>
2024-03-20 16:14:29 +0000

    Skill Menu UI Done
    Ui done and in place now need to add the script and such to be able to control said menu

--- Assets/Scripts/SkillMenu.cs
+++ Assets/Scripts/SkillMenu.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SkillUI : MonoBehaviour
 {
     public GameObject skillmenu;
     public GameObject gameplayui;
+    public GameObject PlayerCamera;
+
+    //Skill levels
+    public TextMeshProUGUI MaxHealthlvl;
+    public TextMeshProUGUI Gatherspeedlvl;
+    public TextMeshProUGUI Strengthlvl;
 
     void Update()
+    {
+        openmenu();
+
+    }
+
+    public void openmenu()
     {
         if (Input.GetKeyDown(KeyCode.P) && !skillmenu.activeSelf)
         {
             skillmenu.SetActive(true);
             gameplayui.SetActive(false);
+            Cursor.lockState = CursorLockMode.Confined;
+            PlayerCamera.GetComponent<CameraRotation>().enabled = false;
         }
         else if (Input.GetKeyDown(KeyCode.P) && skillmenu.activeSelf)
         {
             skillmenu.SetActive(false);
             gameplayui.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+            PlayerCamera.GetComponent<CameraRotation>().enabled = true;
         }
-
     }
+
 }
